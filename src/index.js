bc5e95b6160ca4eb2db6359e154f2379c7cd0c2e
divesh <diveshsisodiads@gmail.com>
2023-10-19 18:16:28 +0530

    Revert "auth service complete"
    This reverts commit 5fa68273d045419052cb725df9bbd96d3a777126.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ const bodyParser = require("body-parser");
 
 const { PORT } = require("./config/serverConfig");
 const apiRoutes = require("./routes/index");
+const UserService = require("./services/user-service");
 
-// const db = require("./models");
+const db = require("./models");
 
 const prepareAndStartServer = async () => {
   const app = express();
@@ -13,6 +14,9 @@ const prepareAndStartServer = async () => {
   app.use(bodyParser.urlencoded({ extended: true }));
 
   app.use("/api", apiRoutes);
+  const service = new UserService();
+
+  const result = await service.isAdmin(1);
 
   app.listen(PORT, () => {
     // db.sequelize.sync({ alter: true });
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -105,47 +105,5 @@ class UserService {
     }
   }
 }
-// TODO
-// createToken(user:{email, id})
-// verifyToken(token)
-// checkPassword(plainPassword,encyptedPassword)
-// async signIn(email, plainPassword)
-// ---- fetch user by email
-// ---- compare passwords
-// ---- generate Token
-// repository --> asycn getByEmail(email)
-
-// user model
-// beforeCreate((user) => {
-// encrypted = bcrypt.hashSync(user.password,SALT) , SALT -> environmen variables -> bcrypt.genSaltSync(9: saltRounds)
-// user.password = encryptedPassword
-// })
-
-// middleware
-// validateUserAuth() -> email or password is missing, commit -> add email and password validation for incoming auth request
-
-// user service
-// async isAuthenticated(token) -> if !response -> err: invalid token, if !user -> err: no user with corresponding token exists
-//  return user.id
-//
-
-// authorisation
-// add model -> role
-// create many to many association user_roles
-// add role seed files
-// create isAdmin method in both service and repository layer
-//
-// middleware
-// validateIsAdminRequest -> !req.body.id -> err: user id is not given, else next();
-//
-// create new micro service -> booking service
-// create error files -> app-error-> genericc, validationError, serviceError
-// create model
-// Booking -> flightId:integer, userId:integer, status:enum,
-// add validations -> flightId -> not null, userId -> not null, status -> not null defauoltValue -> In Process values:['In Process', "Booked", "Cancelled"]
-// commit -> added booking model
-// now add total no. of seats, toatlCost column to model using migration, migration:create
-//
-// flight repository -> add getFlightById
 
 module.exports = UserService;
