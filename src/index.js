5fa68273d045419052cb725df9bbd96d3a777126
divesh <diveshsisodiads@gmail.com>
2023-10-19 18:14:37 +0530

    auth service complete

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ const bodyParser = require("body-parser");
 
 const { PORT } = require("./config/serverConfig");
 const apiRoutes = require("./routes/index");
-const UserService = require("./services/user-service");
 
-const db = require("./models");
+// const db = require("./models");
 
 const prepareAndStartServer = async () => {
   const app = express();
@@ -14,9 +13,6 @@ const prepareAndStartServer = async () => {
   app.use(bodyParser.urlencoded({ extended: true }));
 
   app.use("/api", apiRoutes);
-  const service = new UserService();
-
-  const result = await service.isAdmin(1);
 
   app.listen(PORT, () => {
     // db.sequelize.sync({ alter: true });
